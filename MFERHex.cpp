a88f5b7f492bdd3727ca5855c8b508c22829378c
Sondre Espeland <sondrenoe@gmail.com>
2024-02-02 14:06:36 +0100

    Enums for Lead and Data hex tags
    Created enum classes for Lead (LDN) and Data Type (DTP) hex tags.

--- MFERHex.cpp
+++ MFERHex.cpp
@@ -21,7 +21,8 @@ enum class MWF : unsigned short // Typically 1 byte. Channels can have assigned 
     CHN = 0x05, // Number of channels
     NUL = 0x12, // NULL value (0x8000)
 
-    ATT = 0x3F,     // A channel, next byte in file specifies number (Documentation specifies 0x00 is channel 1, 0x10 is 16 and 0x11 is 17? File starts 0x00 and upwards)
+    ATT = 0x3F, // A channel, next byte in file specifies number
+    // (Documentation specifies 0x00 is Channel 1, 0x10 is 16 and 0x11 is 17? File starts 0x00 and upwards)
     ATT00 = 0x3F00, // Channel 1
     ATT01 = 0x3F01, // Channel 2
     ATT02 = 0x3F02, // Channel 3
@@ -45,15 +46,42 @@ enum class MWF : unsigned short // Typically 1 byte. Channels can have assigned 
     END = 0x80, // End of file
 
     // Channel attributes:
-    LDN = 0x09, // Waveform attributes (Holds Lead)
+    LDN = 0x09, // Waveform attributes (Contains Lead)
     DTP = 0x0A, // Data type
     BLK = 0x04, // Data block length
     // IVL
     SEN = 0x0C, // Sampling resolution
 };
 
-// Lead (Describes Waveform Attributes) Codes. Comments describe their meaning and integer value.
+// Lead (Tag of content of MWF_LDN) codes, in order of documentation.
+// Comments describe their meaning and integer value.
 enum class LDN : unsigned short // 2 bytes
 {
+    STATUS = 0x1040,     // 4160 Status: When status information includes body position
+    BODY_POS = 0x1041,   // 4161 Body position: When body position is output as a continuous waveform
+    BODY_MVMNT = 0x1042, // 4162 Body movement
+    RESP = 0x1043,       // 4163 Respiration: When the respiration measurment method is not specified. Including snoring etc.
+    IRW = 0x00A0,        // 160 Impedance respiration waveform
+    BP = 0x008F,         // 143 Blood pressure: When NIBP or another blood pressure measurement is not specified
+    SPO2 = 0x00AF,       // 175 SpO₂
+    ECG1 = 0x1046,       // 4166 ECG1
+    ECG2 = 0x1047,       // 4167 ECG2
+    ECG3 = 0x1048,       // 4168 ECG3
+    ECG4 = 0x1049,       // 4169 ECG4: Used when the lead name is not clear
+};
 
+// Data Type (Tag of content of MWF_DTP) codes in order of documentation.
+// (OF NOTE, "16 bit signed integer 0 to 65535" has been named Int16U, aka "unsigned")
+enum class DTP : unsigned char // 1 byte
+{
+    INT16S = 0x00,   // 16 bit signed integer -32768 to 32767
+    INT16U = 0x01,   // 16 bit signed integer 0 to 65535 (documented as signed)
+    INT32S = 0x02,   // 32 bit signed integer
+    INT8U = 0x03,    // 8 bit unsigned integer
+    STATUS16 = 0x04, // 16 bit status field
+    INT8S = 0x05,    // 8 bit signed integer
+    INT32U = 0x06,   // 32 bit unsigned integer
+    FLOAT32 = 0x07,  // 32 bit single precision floating point number
+    FLOAT64 = 0x08,  // 64 bit single precision floating point number
+    AHA8 = 0x09,     // 8 bit AHA compression
 };
