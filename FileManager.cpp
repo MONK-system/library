d2c70284e1db17d77e261c8355ddd5fe61e24f91
Sondre Espeland <sondrenoe@gmail.com>
2024-02-16 21:16:36 +0100

    Add file writing methods

--- FileManager.cpp
+++ FileManager.cpp
@@ -1,10 +1,56 @@
 #include "FileManager.h"
+#include <iostream> // For error logging
+
+FileManager::FileManager(const std::string &outputFileName) : fileName(outputFileName), outputFormat("csv")
+{
+    outputFile.open(fileName);
+    if (!outputFile.is_open())
+    {
+        std::cerr << "Error opening output file: " << fileName << std::endl;
+    }
+}
+
+void FileManager::setOutputFormat(const std::string &format)
+{
+    outputFormat = format;
+}
+
+void FileManager::writeLines(const std::vector<std::string> &lines)
+{
+    if (!outputFile.is_open())
+    {
+        std::cerr << "Output file is not open." << std::endl;
+        return;
+    }
+
+    for (const auto &line : lines)
+    {
+        outputFile << line << std::endl;
+    }
+}
+
+void FileManager::closeFile()
+{
+    if (outputFile.is_open())
+    {
+        outputFile.close();
+    }
+}
+
+// Remember to properly close the file when done, which could be handled
+// in the destructor if the file should remain open for the object's lifetime.
+FileManager::~FileManager()
+{
+    closeFile();
+}
 
 // Implementation of readBinaryFile static method
-std::vector<unsigned char> FileManager::readBinaryFile(const std::string& fileName) {
+std::vector<unsigned char> FileManager::readBinaryFile(const std::string &fileName)
+{
     std::ifstream file(fileName, std::ios::binary);
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         throw std::runtime_error("Error opening file.");
     }
 
@@ -15,7 +61,8 @@ std::vector<unsigned char> FileManager::readBinaryFile(const std::string& fileNa
 
     // Assign bytes to vector
     std::vector<unsigned char> dataVector(length);
-    if (!file.read(reinterpret_cast<char *>(dataVector.data()), dataVector.size())) {
+    if (!file.read(reinterpret_cast<char *>(dataVector.data()), dataVector.size()))
+    {
         throw std::runtime_error("Error reading file.");
     }
     file.close();
--- FileManager.h
+++ FileManager.h
@@ -6,9 +6,19 @@
 #include <fstream>
 #include <stdexcept>
 
-class FileManager {
+class FileManager
+{
 public:
-    static std::vector<unsigned char> readBinaryFile(const std::string& fileName);
+    FileManager(const std::string &outputFileName);
+    void setOutputFormat(const std::string &format); // If future file formats are needed
+    void writeLines(const std::vector<std::string> &lines);
+
+    static std::vector<unsigned char> readBinaryFile(const std::string &fileName);
+
+private:
+    std::string fileName;
+    std::string outputFormat;
+    std::ofstream outputFile;
 };
 
 #endif // FILEMANAGER_H
