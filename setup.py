2407c48c9b95c6fa6bc4b2866f15d07d010a0946
Sondre Espeland <sondrenoe@gmail.com>
2024-03-10 15:22:13 +0100

    Refactor + implement setup.py

---
+++ setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import platform
+import subprocess
+import os
+import sys
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('CMake must be installed to build the following extensions: ' +
+                               ', '.join(e.name for e in self.extensions))
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(
+            self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+
+            # Use 'MinGW Makefiles' generators on Windows by default
+            cmake_args += ['-G', 'MinGW Makefiles']
+
+        else:
+            # This will use the default Unix Makefiles on Linux
+            build_args += ['--', '-j' + num_cores]
+
+        os.makedirs(self.build_temp, exist_ok=True)
+        subprocess.check_call(['cmake', ext.sourcedir] +
+                              cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'] +
+                              build_args, cwd=self.build_temp)
+
+
+setup(
+    name='monkLib',
+    version='0.0.1',
+    author='Your Name',
+    description='A test package with a CMake extension',
+    long_description='',
+    ext_modules=[CMakeExtension('monkLib', sourcedir='.')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
