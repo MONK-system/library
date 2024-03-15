ff0d9c883b79dca44f0646f8b7884af01d44a18a
Sondre Espeland <sondrenoe@gmail.com>
2024-03-15 11:38:44 +0100

    Update write.py

--- write.py
+++ write.py
@@ -5,7 +5,7 @@ import argparse
 # Initialize the parser
 parser = argparse.ArgumentParser(description='Plot CSV data')
 # Add the input file as a positional argument
-parser.add_argument('input', help='Input CSV file path')
+parser.add_argument('input', help='Input binary data file path')
 # Add the output file as an positional argument
 parser.add_argument('output', help='Output CSV file path')
 # Parse the command line arguments
