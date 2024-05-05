8d4b412debb66600fa23c661a9e4056845e27af3
Sondre Espeland <sondrenoe@gmail.com>
2024-05-05 19:53:07 +0200

    Implement ctesting + Fix python interpreter errors and linking issues
    Implement CTest using Google Test.
    Fix various issues regarding pybind11 linking & python interpreter errors, where interpreter would be unavailable in scope.

--- core/tests/ByteVector_test.cpp
+++ core/tests/ByteVector_test.cpp
@@ -2,7 +2,8 @@
 #include "ByteVector.h"
 
 // Test for stringToEncoding function
-TEST(ByteVectorTest, StringToEncoding) {
+TEST(ByteVectorTest, StringToEncoding)
+{
     EXPECT_EQ(Encoding::ASCII, stringToEncoding("ASCII"));
     EXPECT_EQ(Encoding::UTF8, stringToEncoding("UTF-8"));
     EXPECT_EQ(Encoding::UTF16LE, stringToEncoding("UTF-16LE"));
@@ -10,27 +11,33 @@ TEST(ByteVectorTest, StringToEncoding) {
 }
 
 // Test for stringify method
-TEST(ByteVectorTest, Stringify) {
+TEST(ByteVectorTest, Stringify)
+{
     ByteVector bv = {0xAB, 0xCD, 0x45};
     EXPECT_EQ("AB CD 45", bv.stringify());
 }
 
 // Test for toString method
-TEST(ByteVectorTest, ToString) {
-    ByteVector bv = {0x48, 0x65, 0x6C, 0x6C, 0x6F};  // 'Hello' in ASCII
+TEST(ByteVectorTest, ToString)
+{
+    ByteVector bv = {0x48, 0x65, 0x6C, 0x6C, 0x6F}; // 'Hello' in ASCII
     EXPECT_EQ("Hello", bv.toString(Encoding::ASCII));
-    EXPECT_THROW(bv.toString(Encoding::UTF16LE), std::runtime_error); // Handling binary to string conversion error for unsupported encoding in test case
+    EXPECT_THROW(bv.toString(Encoding::UTF16LE), std::runtime_error);  // Handling binary to string conversion error for unsupported encoding
+    bv = {0x48, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F, 0x00}; // 'Hello' in UTF-16LE
+    EXPECT_EQ("Hello", bv.toString(Encoding::UTF16LE));
 }
 
 // Test for toInt method
-TEST(ByteVectorTest, ToInt) {
+TEST(ByteVectorTest, ToInt)
+{
     ByteVector bv = {0x00, 0x00, 0x01, 0x02};
     EXPECT_EQ(258, bv.toInt<uint32_t>());
-    EXPECT_THROW(bv.toInt<int8_t>(), std::runtime_error);  // Test for size larger than type
+    EXPECT_THROW(bv.toInt<int8_t>(), std::runtime_error); // Test for size larger than type
 }
 
 // Test for fromInt method
-TEST(ByteVectorTest, FromInt) {
+TEST(ByteVectorTest, FromInt)
+{
     auto bv = ByteVector::fromInt(258);
     ByteVector expected = {0x01, 0x02};
     EXPECT_EQ(expected, bv);
@@ -40,7 +47,8 @@ TEST(ByteVectorTest, FromInt) {
     EXPECT_EQ(expected, bv);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
